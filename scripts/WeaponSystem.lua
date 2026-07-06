7545de970ce6485764854468c26d0cd43a5445ea
sithstudios9-hub <sithstudios9@gmail.com>
2026-07-05 22:38:32 -0400

    Create WeaponSystem.lua

---
+++ scripts/WeaponSystem.lua
@@ -0,0 +1,12 @@
+-- scripts/WeaponSystem.lua
+local WeaponSystem = {}
+
+function WeaponSystem.GetDamage()
+    return 50
+end
+
+function WeaponSystem.SetDamage(damage)
+    -- logic here
+end
+
+return WeaponSystem
